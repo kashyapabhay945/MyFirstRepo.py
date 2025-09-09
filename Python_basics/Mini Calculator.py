a599d4992bbb0db2ed9b68b1538e64308fdfd951
Abhay Kashyap <kashyap.abhay945@outlook.com>
2025-09-09 16:49:01 +0530

    Added a basic arithmetic project using python

---
+++ Python_basics/Mini Calculator.py
@@ -0,0 +1,9 @@
+a=int(input("First Integer: "))
+b=int(input("Second Integer: "))
+print("\nAddition of both the integers", "is:",a + b) #'\n' is an escape sequence which help in running the code from the next like (one-line gap)
+print("Subtraction of both the integers", "is:",a - b)
+print("Multiplication of both the integers", "is:",a * b)
+print("Division of both the integers", "is:", a / b)
+print("Modulus of both the integers", "is:", a % b) #it returns the reminder of the division
+print("Exponential of both the integers", "is:", a * b) #it raises first integer to the power of second integer
+print("Floor division of both the integers", "is:", a * b) #it returns the integer part of the quotient
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MyFirstRepo(.py)
-This is my first repository created while learning python. It will include some python codes. I am open for any suggestions with respect to any improvement advice which will be helpful in enhancing my knowledge in this field.
+This is my first repository created while learning python. It will include python projects. I am open for any suggestions with respect to any improvement which will be helpful in enhancing my knowledge in this field.
 <br>
 Thanks,<br>
 Abhay.
